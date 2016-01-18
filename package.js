2efa0315113c46332c4259e0217772f43c4829cf
cristo-rabani <cristo.rabani@gmail.com>
2016-01-18 19:45:00 +0100

    ups

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'universe:i18n',
-    version: '1.3.1',
+    version: '1.3.2',
     summary: 'React i18n (also es6 modules), Both YAML & JSON translation files supported. recognizes 353 locales',
     git: 'https://github.com/vazco/meteor-universe-i18n'
 });
@@ -19,7 +19,6 @@ Npm.depends({
     'yamljs': '0.2.4'
 });
 
-
 Package.onUse(function (api) {
     api.versionsFrom('1.2.1');
 
@@ -33,8 +32,9 @@ Package.onUse(function (api) {
         'isobuild:compiler-plugin@1.0.0'
     ]);
 
-    api.use('universe:modules@0.6.7', {weak:true});
+    api.use('universe:modules@0.6.8', {weak:true});
     api.use('react-runtime@0.13.3_6||0.14.1', {weak:true});
+    api.use('react@0.13.3_6||0.14.1', {weak:true});
 
     api.addFiles([
         'lib/locales.js',

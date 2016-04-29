376b751e9b23868531e0fb1a898762c0f8dc5fc6
cristo-rabani <cristo.rabani@gmail.com>
2016-04-29 22:08:04 +0200

    contextMustBeInFiber

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'universe:i18n',
-    version: '1.5.0',
+    version: '1.5.1',
     summary: 'Lightweight i18n, YAML & JSON translation files, React component, incremental & remote loading',
     git: 'https://github.com/vazco/meteor-universe-i18n'
 });

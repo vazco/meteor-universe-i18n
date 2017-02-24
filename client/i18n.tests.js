a09721e6fe952f668425579131cc90404ec4aa2a
Jan <jan.owsiany@vazco.eu>
2017-02-24 16:35:44 +0100

    Fix brackets

--- client/i18n.tests.js
+++ client/i18n.tests.js
@@ -1,6 +1,6 @@
 describe('universe-i18n - client', function () {
     it('should load translations incrementally', async function () {
-        expect(i18n.isLoaded['it-IT']).to.not.be.true;
+        expect(i18n.isLoaded('it-IT')).to.not.be.true;
         await i18n.setLocale('it-IT');
         expect(i18n.isLoaded('it-IT')).to.be.true;
         expect(i18n.getLocale()).to.equal('it-IT');

c36484852da46c6e0cb751b2303d86806d665217
Jan <jan.owsiany@vazco.eu>
2017-02-25 14:40:19 +0100

    Add locale to available translations only if translation is not an empty JSON

--- server/api.js
+++ server/api.js
@@ -57,12 +57,13 @@ function getJS (locale, namespace, isBefore) {
         if (isBefore) {
             return `var w=this||window;w.__uniI18nPre=w.__uniI18nPre||{};w.__uniI18nPre['${locale}.${namespace}'] = ${json}`;
         }
-        return `(Package['universe:i18n'].i18n).addTranslations('${locale}', '${namespace}', ${json});`;
+        return json.length > 2 ? `(Package['universe:i18n'].i18n).addTranslations('${locale}', '${namespace}', ${json});` : '';
     }
     if (isBefore) {
         return `var w=this||window;w.__uniI18nPre=w.__uniI18nPre||{};w.__uniI18nPre['${locale}'] = ${json}`;
     }
-    return `(Package['universe:i18n'].i18n).addTranslations('${locale}', ${json});`;
+
+    return json.length > 2 ? `(Package['universe:i18n'].i18n).addTranslations('${locale}', ${json});` : '';
 }
 
 i18n._formatgetters = {getJS, getJSON, getYML};

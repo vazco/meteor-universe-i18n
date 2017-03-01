f7de33235e75de507c19f3f3f6923aa353ea83ae
Jan <jan.owsiany@vazco.eu>
2017-03-02 00:19:58 +0100

    Add server side tests

--- tests/client/i18n.tests.js
+++ tests/client/i18n.tests.js
@@ -1,12 +1,12 @@
-describe('universe-i18n - client', function () {
-    it('should load translations incrementally', async function () {
+describe('universe-i18n - client', () => {
+    it('should load translations incrementally', async () => {
         expect(i18n.isLoaded('it-IT')).to.not.be.true;
         await i18n.setLocale('it-IT');
         expect(i18n.isLoaded('it-IT')).to.be.true;
         expect(i18n.getLocale()).to.equal('it-IT');
     });
 
-    it('should list only available languages', async function () {
+    it('should list only available languages', async () => {
         expect(i18n.isLoaded('zh-CN')).to.not.be.true;
         await i18n.setLocale('zh-CN');
         expect(i18n.isLoaded('zh-CN')).to.be.true;
--- tests/server/i18n.tests.js
+++ tests/server/i18n.tests.js
@@ -1,1 +1,22 @@
+describe('universe-i18n - client', () => {
+    it('should return object from getCache', () => {
+        expect(i18n.getCache()).to.be.an('object');
+        expect(i18n.getCache('en-US')).to.be.an('object');
+        expect(i18n.getCache('it-IT')).to.be.an('object');
+    });
+
+    it('should be able to call getYML', () => {
+        const cache = i18n.getCache('it-IT');
+        expect(cache).to.be.an('object');
+        expect(cache.getYML('it-IT')).to.be.a('string');
+    });
+
+    it('should be able to call getJSON', () => {
+        const cache = i18n.getCache('es-ES');
+        expect(cache).to.be.an('object');
+        expect(cache.getYML('es-ES')).to.be.a('string');
+    });
+});
+
+
 import '../../i18n.tests';

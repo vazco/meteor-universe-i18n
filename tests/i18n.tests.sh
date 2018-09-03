6a8e51883ce52c96fc1685fa2ae3fd049c2e3d60
Cristo <cristo.rabani@gmail.com>
2018-09-03 20:53:51 +0200

    cultofcoders:mocha

--- tests/i18n.tests.sh
+++ tests/i18n.tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-UNIVERSE_I18N_LOCALES="en-US" meteor test-packages --settings tests/settings.coverage.json --port 8080 --driver-package practicalmeteor:mocha ./
+UNIVERSE_I18N_LOCALES="en-US" meteor test-packages --settings tests/settings.coverage.json --port 8080 --driver-package cultofcoders:mocha ./

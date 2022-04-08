41b6f018e384b323f0b3f17061aceee63d0cb9e1
Piotr Pośpiech <ptkpospiech@gmail.com>
2022-04-08 11:18:36 +0200

    removed unused type

--- source/common.ts
+++ source/common.ts
@@ -4,10 +4,6 @@ import { Tracker } from 'meteor/tracker';
 
 import { JSON, JSONObject, get, isJSONObject, set } from './utils';
 
-export interface CreateTranslatorOptions extends GetTranslationOptions {
-  _namespace?: string;
-}
-
 export interface GetCacheEntry {
   getJS(locale: string, namespace: string, isBefore?: boolean): string;
   getJSON(locale: string, namespace: string, diff?: string): string;

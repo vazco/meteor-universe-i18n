44db4f1aabd159edfe8c3792d65ed52521814b3b
Piotr Pośpiech <ptkpospiech@gmail.com>
2022-07-20 13:08:00 +0200

    refactor

--- types/utils.d.ts
+++ types/utils.d.ts
@@ -1,10 +1,22 @@
-export declare type JSON = boolean | null | number | string | JSON[] | JSONObject;
+export declare type JSON =
+  | boolean
+  | null
+  | number
+  | string
+  | JSON[]
+  | JSONObject;
 export declare type JSONObject = {
-    [key: string]: JSON;
+  [key: string]: JSON;
 };
 declare type UnknownRecord = Record<string, unknown>;
 export declare function get(object: UnknownRecord, path: string): unknown;
-export declare function isJSONObject(value: JSON | unknown): value is JSONObject;
-export declare function set(object: UnknownRecord, path: string, value: unknown): void;
+export declare function isJSONObject(
+  value: JSON | unknown,
+): value is JSONObject;
+export declare function set(
+  object: UnknownRecord,
+  path: string,
+  value: unknown,
+): void;
 export {};
 //# sourceMappingURL=utils.d.ts.map

933192f74ba606ade9da829e0e07c5380bac6c4d
Michał Zendran <zendran.m@gmail.com>
2024-09-13 14:07:43 +0200

    Replacing chai expect with nodejs assert

--- tests/client.ts
+++ tests/client.ts
@@ -1,33 +1,21 @@
-// import { i18n } from '../source/client';
+import { i18n } from '../source/client';
+import assert from 'assert';
 
-// const expect = chai.expect;
-
-// describe('universe-i18n - client', () => {
-//   it('should load translations incrementally', async () => {
-//     const locale = 'it-IT';
-//     expect(i18n.isLoaded(locale)).to.not.be.true;
-//     await i18n.setLocale(locale);
-//     expect(i18n.isLoaded(locale)).to.be.true;
-//     expect(i18n.getLocale()).to.equal(locale);
-//   });
-
-//   it('should list only available languages', async () => {
-//     const locale = 'zh-CN';
-//     expect(i18n.isLoaded(locale)).to.not.be.true;
-//     await i18n.setLocale(locale);
-//     expect(i18n.isLoaded(locale)).to.be.true;
-//     expect(i18n.getLocale()).to.equal(locale);
-//   });
-// });
-
-describe('my module', function () {
-  it('does something that should be tested', function () {
-    // This code will be executed by the test driver when the app is started
-    // in the correct mode
+describe('universe-i18n - client', () => {
+  it('should load translations incrementally', async () => {
+    const locale = 'it-IT';
+    assert.notEqual(i18n.isLoaded(locale), true);
+    await i18n.setLocale(locale);
+    assert.equal(i18n.isLoaded(locale), true);
+    assert.equal(i18n.getLocale(), locale);
   });
 
-  it('Aaaaaa', function () {
-    // This code will be executed by the test driver when the app is started
-    // in the correct mode
+  it('should list only available languages', async () => {
+    const locale = 'zh-CN';
+    assert.notEqual(i18n.isLoaded(locale), true);
+    await i18n.setLocale(locale);
+    assert.equal(i18n.isLoaded(locale), true);
+    assert.equal(i18n.getLocale(), locale);
+    assert.strictEqual(!i18n.getLocales().includes(i18n.getLocale()), true);
   });
 });

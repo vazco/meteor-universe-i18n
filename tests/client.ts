24931a3cb41b98359b8586efd8821794a4a30cb0
Michał Zendran <zendran.m@gmail.com>
2024-09-13 12:05:08 +0200

    Testing new working configuration

--- tests/client.ts
+++ tests/client.ts
@@ -1,21 +1,33 @@
-import { i18n } from '../source/client';
+// import { i18n } from '../source/client';
 
-const expect = chai.expect;
+// const expect = chai.expect;
 
-describe('universe-i18n - client', () => {
-  it('should load translations incrementally', async () => {
-    const locale = 'it-IT';
-    expect(i18n.isLoaded(locale)).to.not.be.true;
-    await i18n.setLocale(locale);
-    expect(i18n.isLoaded(locale)).to.be.true;
-    expect(i18n.getLocale()).to.equal(locale);
+// describe('universe-i18n - client', () => {
+//   it('should load translations incrementally', async () => {
+//     const locale = 'it-IT';
+//     expect(i18n.isLoaded(locale)).to.not.be.true;
+//     await i18n.setLocale(locale);
+//     expect(i18n.isLoaded(locale)).to.be.true;
+//     expect(i18n.getLocale()).to.equal(locale);
+//   });
+
+//   it('should list only available languages', async () => {
+//     const locale = 'zh-CN';
+//     expect(i18n.isLoaded(locale)).to.not.be.true;
+//     await i18n.setLocale(locale);
+//     expect(i18n.isLoaded(locale)).to.be.true;
+//     expect(i18n.getLocale()).to.equal(locale);
+//   });
+// });
+
+describe('my module', function () {
+  it('does something that should be tested', function () {
+    // This code will be executed by the test driver when the app is started
+    // in the correct mode
   });
 
-  it('should list only available languages', async () => {
-    const locale = 'zh-CN';
-    expect(i18n.isLoaded(locale)).to.not.be.true;
-    await i18n.setLocale(locale);
-    expect(i18n.isLoaded(locale)).to.be.true;
-    expect(i18n.getLocale()).to.equal(locale);
+  it('Aaaaaa', function () {
+    // This code will be executed by the test driver when the app is started
+    // in the correct mode
   });
 });
